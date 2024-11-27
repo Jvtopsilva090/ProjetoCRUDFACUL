999986e39e3e4982063a9f6c43b2af21290b8b59
Shelby_0901 <94905609+Jvtopsilva090@users.noreply.github.com>
2024-11-27 18:11:08 -0300

    dummy_usuarios.dart
    Atualizado
    
    Signed-off-by: Shelby_0901 <94905609+Jvtopsilva090@users.noreply.github.com>

---
+++ lib/data/dummy_usuarios.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_crud/models/usuario.dart';
+
+// ignore: constant_identifier_names
+const DUMMY_USERS = {
+  '1': Usuario(id: '1', nome: 'Maria', email: 'testemaria@teste.com', numero: '12345678910', icone: 'https://cdn.pixabay.com/photo/2021/04/20/07/59/woman-6193184_1280.jpg'),
+  '2': Usuario(id: '2', nome: 'João', email: 'testejoao@teste.com', numero: '12345678910', icone: 'https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_1280.jpg'),
+  '3': Usuario(id: '3', nome: 'Breno', email: 'testebreno@teste.com', numero: '12345678910', icone: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_1280.png'),
+  '4': Usuario(id: '4', nome: 'Teste', email: 'teste@teste.com', numero: '12345678910', icone: ''),
+  '5': Usuario(id: '5', nome: 'Jorge', email: 'jorge2929@gmail.com', numero: '12345678910', icone: ''),
+  '6': Usuario(id: '6', nome: 'Pedro', email: 'pedrouhhgs215@gmail.com', numero: '12345678910', icone: ''),
+  '7': Usuario(id: '7', nome: 'Patricia', email: 'pati123@gmail.com', numero: '12345678910', icone: ''),
+  '8': Usuario(id: '8', nome: 'karine', email: 'karinedatropa@gmail.com', numero: '12345678910', icone: ''),
+};
