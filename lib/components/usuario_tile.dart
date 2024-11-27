6108df2660bc3b9035e17ffeb526bc54a9137347
Shelby_0901 <94905609+Jvtopsilva090@users.noreply.github.com>
2024-11-27 18:13:09 -0300

    usuario_tile.dart
    Atualizado
    
    Signed-off-by: Shelby_0901 <94905609+Jvtopsilva090@users.noreply.github.com>

---
+++ lib/components/usuario_tile.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/components/notificacao.dart';
+import 'package:flutter_crud/models/usuario.dart';
+import 'package:flutter_crud/provider/usuariosProvider.dart';
+import 'package:flutter_crud/routes/app_routes.dart';
+import 'package:provider/provider.dart';
+
+class UserTile extends StatelessWidget {
+  final Usuario user;
+
+  const UserTile(this.user, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final avatar = user.icone.isEmpty
+        ? const CircleAvatar(child: Icon(Icons.person))
+        : CircleAvatar(backgroundImage: NetworkImage(user.icone));
+
+    return ListTile(
+      leading: avatar,
+      title: Text(user.nome),
+      subtitle: Text(user.email),
+      trailing: SizedBox(
+        width: 100,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.edit),
+              color: Colors.orange,
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamed(AppRoutes.USER_EDIT_FORM, arguments: user);
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.delete),
+              color: Colors.red,
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                          title: const Text('Excluir Usuário'),
+                          content: const Text(
+                              'Tem certeza que deseja excluir este usuário?'),
+                          actions: <Widget>[
+                            TextButton(
+                              child: const Text('Não'),
+                              onPressed: () => Navigator.of(context).pop(false),
+                            ),
+                            TextButton(
+                              child: const Text('Sim'),
+                              onPressed: () {
+                                Provider.of<Users>(context, listen: false)
+                                    .remove(user);
+                                Navigator.of(context).pop(true);
+                                Notify.show(context, 'Usuário deletado com sucesso!');
+                              },
+                            ),
+                          ],
+                        ));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
