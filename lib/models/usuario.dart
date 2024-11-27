0f7aaa94e6b49817daec08edc7a0c94747d1ddcd
Shelby_0901 <94905609+Jvtopsilva090@users.noreply.github.com>
2024-11-27 18:07:19 -0300

    usuario.dart
    Atualizado
    
    Signed-off-by: Shelby_0901 <94905609+Jvtopsilva090@users.noreply.github.com>

--- lib/models/usuario.dart
+++ lib/models/usuario.dart
@@ -1,37 +1,15 @@
 class Usuario {
-  final int id;
+  final String? id;
   final String nome;
   final String email;
-  final String numero; //+55 (62) 91234-1234
-  final int icone;
+  final String numero;
+  final String icone;
 
   const Usuario({
-    this.id = 0,
+    this.id,
     required this.nome,
     required this.email,
     required this.numero,
     required this.icone,
   });
-
-  Map<String, Object?> toMap() {
-    return {
-<<<<<<< HEAD
-      //'id': this.id,
-      'nome': this.nome,
-      'email': this.email,
-      'numero': this.numero,
-      'icone': this.icone,
-=======
-      'nome': nome,
-      'email': email,
-      'numero': numero,
-      'icone': icone,
->>>>>>> d2b590c (Descrição das mudanças realizadas)
-    };
-  }
-
-  @override
-  String toString() {
-    return 'Usuario{id: $id, nome: $nome, email: $email, numero: $numero, icone: $icone}';
-  }
 }
