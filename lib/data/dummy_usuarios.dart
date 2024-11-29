c6e7177129bc494513edbf51779634474186b44b
Shelby_0901 <94905609+Jvtopsilva090@users.noreply.github.com>
2024-11-29 20:46:20 -0300

    dummy_usuarios.dart
    Atualizado
    
    Signed-off-by: Shelby_0901 <94905609+Jvtopsilva090@users.noreply.github.com>

--- lib/data/dummy_usuarios.dart
+++ lib/data/dummy_usuarios.dart
@@ -6,7 +6,7 @@ const DUMMY_USERS = {
   '2': Usuario(id: '2', nome: 'João', email: 'testejoao@teste.com', numero: '12345678910', icone: 'https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_1280.jpg'),
   '3': Usuario(id: '3', nome: 'Breno', email: 'testebreno@teste.com', numero: '12345678910', icone: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_1280.png'),
   '4': Usuario(id: '4', nome: 'Teste', email: 'teste@teste.com', numero: '12345678910', icone: ''),
-  '5': Usuario(id: '5', nome: 'Jorge', email: 'jorge2929@gmail.com', numero: '12345678910', icone: ''),
+  '5': Usuario(id: '5', nome: 'Shelby ne vida', email: 'shelbynevida@gmail.com', numero: '7777777777', icone: 'https://miro.medium.com/v2/resize:fit:2400/1*k38PmUEbJR64Tbdz6sPNjA.jpeg'),
   '6': Usuario(id: '6', nome: 'Pedro', email: 'pedrouhhgs215@gmail.com', numero: '12345678910', icone: ''),
   '7': Usuario(id: '7', nome: 'Patricia', email: 'pati123@gmail.com', numero: '12345678910', icone: ''),
   '8': Usuario(id: '8', nome: 'karine', email: 'karinedatropa@gmail.com', numero: '12345678910', icone: ''),
